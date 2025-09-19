7825b58736d1b2d7a10f8fdf29196f7c0f85a95e
oatsu <oatsu.dev@gmail.com>
2025-09-19 22:10:19 +0900

    __init__ を修正

--- PyRwu/effects/__init__.py
+++ PyRwu/effects/__init__.py
@@ -1,6 +1,23 @@
-import os
+from . import (
+    base,
+    eb_flag,
+    g_flag,
+    gw_flag,
+    large_a_flag,
+    large_b_flag,
+    large_p_flag,
+    t_flag,
+    vf_flag,
+)
 
 __all__ = [
-    k[0:-3]
-    for k in filter(lambda x: x[0] != "_", os.listdir(os.path.dirname(__file__)))
-]  # pyright: ignore[reportUnsupportedDunderAll]
+    'base',
+    'eb_flag',
+    'g_flag',
+    'gw_flag',
+    'large_a_flag',
+    'large_b_flag',
+    'large_p_flag',
+    't_flag',
+    'vf_flag',
+]
